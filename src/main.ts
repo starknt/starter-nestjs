6b36b0b4f40ad95c7b463ba903de2244c33d084e
starknt <1431880400@qq.com>
2022-07-22 09:25:44 +0800

    chore: init prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import PrimaService from './services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrimaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,1 +1,14 @@
-import { } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+import { INestApplication, OnModuleInit } from '@nestjs/common';
+
+export default class PrimaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
