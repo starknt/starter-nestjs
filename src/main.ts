88b5b98ef748ab6f849ce201cba3f3372b0e09dd
starknt <1431880400@qq.com>
2022-07-24 22:30:40 +0800

    chore: listen

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import PrimaService from './services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
+  const host = configService.get('environment.host')
+  const port = configService.get('environment.port')
 
   const prismaService = app.get(PrimaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(port, host);
+
+  console.log(`\x1b[93m Run in http://${host}:${port}`)
 }
 bootstrap();
